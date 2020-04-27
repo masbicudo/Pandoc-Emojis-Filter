cb573aee27609709fad31880489ea3533b4a7ffa
Miguel Angelo <masbicudo@gmail.com>
2020-04-27 02:42:08 -0300

    Adding link URIs as footnotes

--- create-example.sh
+++ create-example.sh
@@ -1,4 +1,5 @@
+format=pdf
 npm install
-
-pandoc --template="template.tex" -o example.pdf readme.md \
-    --filter=emoji_filter.js -M emoji=noto-emoji --from gfm
+pandoc --template="template.tex" -o example.$format readme.md \
+    --filter=emoji_filter.js -M emoji=noto-emoji --from gfm \
+    -V links-as-notes=true -V colorlinks -V urlcolor=NavyBlue
